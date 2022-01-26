c0a2e489b4bdd5ba02c5b4920ded648b0ba25df0
SHREYA MALOGI <5hre9a@gmail.com>
2022-01-26 22:58:41 +0530

    version 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,11 +3,27 @@ import React from "react";
 import Header from "./Header";    
 import Footer from "./Footer";
 import Note from "./Note";
- 
+import notes from "../notes";
+
+//keep a function which cretes notes and we gonna pass a single note item into it and renders a custom  note component
+//and which will return a note component which will have props
+//this title and content props must be there is notes.js
+//Whenever we want to loop through or map through a dynamic array we must have a key
+
+
 function App(){
-    return (<div>
+    return (
+        <div>
         <Header />
-        <Note />
+        
+        {notes.map ((noteItem) =>
+        <Note
+         key = {noteItem.key}
+         title = {noteItem.title}   
+         content = {noteItem.content}
+        />
+    )}
+
         <Footer />
     </div>
     );
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-function Note(){
+function Note(props){
     return (
     <div className = "note"> 
 
-    <h1>This is the title </h1>
-    <p> This is the content </p>
+    <h1>{props.title} </h1>
+    <p> {props.content} </p>
     
     </div>
     )
